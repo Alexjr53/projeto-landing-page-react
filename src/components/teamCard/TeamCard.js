5783646ef706eb32efc8647c37368a310a88f61d
Alexsandro <alexsandrojunior53@gmail.com>
2024-08-31 11:29:21 -0300

    component TeamCard criado

---
+++ src/components/teamCard/TeamCard.js
@@ -0,0 +1,26 @@
+import React from "react"
+import './TeamCard.css'
+
+function TeamCard({imageUrl, name, position, description}){
+    return(
+        <div className="teamMember">
+            
+            <div className="topBoxUser">
+              <div className="boxAvatar">
+                <img src={imageUrl} alt={name} />
+              </div>
+
+              <div className="infoUser">
+                <p className='memberName'>{name}</p>
+                <p className='memberDescription' >{position}</p>
+              </div>
+            </div>
+
+            <div className="descriptionUserBox">
+              <p>{description}</p>
+            </div>
+        </div>
+    )
+}
+
+export default TeamCard
