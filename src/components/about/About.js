2d32e05d72314f1e4236656ad0cc159921976bbf
Alexsandro <alexsandrojunior53@gmail.com>
2024-08-31 11:30:19 -0300

    estilo alterado

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,11 +1,13 @@
-import BackHomeButton from '../button/BackHomeButton';
 import './About.css'
-import Team from '../team/Team'
+import TeamCard from '../teamCard/TeamCard';
+import avatar1 from '../../assets/images/avatar.png';
+import avatar2 from '../../assets/images/avatar-2.png';
+import avatar3 from '../../assets/images/avatar-3.png';
+import avatar4 from '../../assets/images/avatar-4.png';
 
 function About() {
   return (
     <section className="aboutSection">
-      <BackHomeButton />
       <div className='aboutContent'>
         <h2 className='aboutTitle'>Sobre nossa <span>empresa</span></h2>
         <p className='aboutText'>Fundada em [Ano], nossa empresa começou com o objetivo de [Objetivo Inicial]. Ao longo dos anos, conquistamos vários marcos importantes, incluindo [Marcos Importantes].</p>
@@ -26,7 +28,35 @@ function About() {
           </ul>
         </div>
       </div>
-      <Team />
+      <section className="teamSection">
+        <h2 className='title'><div className="borderEffect"></div>Conheça nosso time!</h2>
+        <div className="teamContainer">
+          <TeamCard 
+            name="Alexsandro Rosa Junior"
+            position="CEO / Instrutor"
+            description="Nossa abordagem é centrada no cliente. Trabalhamos em estreita colaboração para garantir que cada solução desenvolvida esteja alinhada com os objetivos de negócio e gere valor real."
+            imageUrl={avatar1}
+          />
+          <TeamCard 
+            name="Maria Silva"
+            position="Diretora de Operações"
+            description="Maria é a responsável por supervisionar as operações diárias da empresa. Com um foco rigoroso na eficiência e qualidade, ela garante que todos os projetos sejam entregues dentro do prazo e excedam as expectativas dos clientes."
+            imageUrl={avatar2}
+          />
+          <TeamCard 
+            name="João Pereira"
+            position="Designer UX/UI"
+            description="Especialista em criar interfaces que garantem uma experiência fluida e eficiente para o usuário. Trabalha em conjunto com a equipe de desenvolvimento para transformar ideias em produtos digitais incríveis."
+            imageUrl={avatar3}
+          />
+          <TeamCard 
+            name="Carla Oliveira"
+            position="Coordenadora de Marketing"
+            description="Carla lidera as iniciativas de marketing da empresa, criando estratégias que fortalecem a marca e aumentam a visibilidade no mercado. Sua abordagem inovadora garante que nossas campanhas sejam sempre eficazes e impactantes."
+            imageUrl={avatar4}
+          />
+        </div>
+      </section>
     </section>
   );
 }
---
+++ src/components/navMenu/NavMenu.js
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom"
+import './NavMenu.css'
+
+function NavMenu({className}){
+    return(
+        <ul className={className}>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">Sobre</Link></li>
+            <li><Link to="/product">Produtos</Link></li>
+            <li><Link to="/contact">Contato</Link></li>
+        </ul>
+    )
+}
+
+export default NavMenu
